0afb04e3f1605b1e523e47757aa703cd88c2c66a
henritresor <shimwamanahenritresor@gmail.com>
2024-10-02 14:09:49 +0200

    removed lorem placeholders

--- components/Landing/AboutUs/index.tsx
+++ components/Landing/AboutUs/index.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function AboutUs({ }) {
   return (
     <div
-    id='about-us'
+      id='about-us'
       className='w-full p-10 bg-[var(--color-2)] text-black h-auto flex items-center justify-evenly'
     >
 
@@ -20,7 +20,7 @@ function AboutUs({ }) {
       </div>
 
       <div className='w-full sm:w-1/2 flex flex-col gap-5 items-start'>
-      <div className='flex items-center gap-5'>
+        <div className='flex items-center gap-5'>
           <Image
             src="/svg/logo_blue.svg"
             width={50}
@@ -35,9 +35,9 @@ function AboutUs({ }) {
             alt='About Us, Brop Inc'
             className=''
           />
-      </div>
+        </div>
 
-        <p className='leading-8 text-black'>At Brop Co, we are passionate about leveraging technology to transform businesses. Based in Kigali, Rwanda, we offer a full range of software development, web services, branding, and innovative engineering solutions. Our team of seasoned experts combines creativity with technical expertise to deliver top-notch digital solutions tailored to our clients' specific needs. We believe in building lasting partnerships by turning ideas into reality, using cutting-edge technology and a customer-first approach. Whether it's developing mobile apps, designing impactful websites, or integrating AI and IoT into your operations, Brop Co is dedicated to driving your business towards success in the digital age.</p>
+        <p className='leading-8 text-black'>At Brop Co, we are passionate about leveraging technology to transform businesses. Based in Kigali, Rwanda, we offer a full range of software development, web services, branding, and innovative engineering solutions. Our team of seasoned experts combines creativity with technical expertise to deliver top-notch digital solutions tailored to our clients&apos; specific needs. We believe in building lasting partnerships by turning ideas into reality, using cutting-edge technology and a customer-first approach. Whether it&apos;s developing mobile apps, designing impactful websites, or integrating AI and IoT into your operations, Brop Co is dedicated to driving your business towards success in the digital age.</p>
 
         <button className='btn btn--unusual'>
           Contact Us
